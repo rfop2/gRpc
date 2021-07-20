24556886ae67c463f38dab0c258cf8d42988f984
rodrigo <rfop@cin.ufpe.br>
2021-07-20 17:29:56 -0300

    requisicoes e tempototal

--- client/client.go
+++ client/client.go
@@ -3,32 +3,44 @@ package main
 import (
 	"context"
 	"fmt"
-	"project-go/pb"
-	"google.golang.org/grpc"
 	"log"
+	"project-go/pb"
 	"time"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
 	fmt.Println("Oi do cliente ...")
 
+	tempoTotal := 0.0
+
 	opts := grpc.WithInsecure()
-	cc, err := grpc.Dial("localhost:50051", opts)
+	cc, err := grpc.Dial("192.168.1.82:50051", opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer cc.Close()
 
-	start := time.Now()
+	requisicoes := 1000
 
-	client := pb.NewFactorialServiceClient(cc)
-	request := &pb.FactorialRequest{Numero: "20"}
+	for i := 0; i <= requisicoes; i++ {
+		start := time.Now()
 
-	resp, _ := client.Factorial(context.Background(), request)
-	end := time.Now()
+		client := pb.NewFactorialServiceClient(cc)
+		request := &pb.FactorialRequest{Numero: "20"}
 
-	fmt.Printf("Resultado recebido => [%v]", resp.Resultado)
-	
-	tempo := end.Sub(start)
-	fmt.Println("tempo: ", tempo.Seconds())
+		resp, _ := client.Factorial(context.Background(), request)
+		end := time.Now()
+
+		fmt.Printf("Resultado recebido => [%v]", resp.Resultado)
+
+		tempo := end.Sub(start)
+		fmt.Println("tempo em segundos: ", tempo.Seconds())
+
+		tempoTotal = tempo.Seconds() + tempoTotal
+	}
+
+	fmt.Println(" media: ", tempoTotal/float64(requisicoes))
+
 }
